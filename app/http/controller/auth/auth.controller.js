8857f04e3a302ba77ebb92d3cb89c898e61ad5c7
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-29 02:29:23 +0330

    create and handle get otp

---
+++ app/http/controller/auth/auth.controller.js
@@ -0,0 +1,88 @@
+const createHttpError = require("http-errors");
+const { RandomNumberGenerator } = require("../../../utils/functions.utils");
+const UserModel = require("../../models/user/user.model");
+const { GetOtpValidation } = require("../../validation/auth/auth.validation");
+const { StatusCodes: HttpStatus } = require("http-status-codes");
+const Controller = require("../controller");
+
+class Auth extends Controller {
+  async getOtp(req, res, next) {
+    try {
+      const authValidation = await GetOtpValidation.validateAsync(req.body);
+      const { mobile } = authValidation;
+      const code = RandomNumberGenerator();
+      const result = await this.saveUser(code, mobile);
+      if (!result) throw createHttpError.Unauthorized("ورود شما انجام نشد");
+      return res.status(HttpStatus.CREATED).json({
+        statusCode: HttpStatus.CREATED,
+        data: {
+          message: "ورود شما با موفقیت انجام شد",
+          code,
+          mobile,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async checkOtp(req, res, next) {
+    try {
+        
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async refreshToken(req, res, next) {
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async saveUser(code, mobile) {
+    const now = new Date().getTime();
+    let otp = {
+      code,
+      expiresIn: now + 2592000,
+    };
+    const user = await this.checkExistUser(mobile);
+    const countOfRegisteredUser = await UserModel.count();
+    if (user) {
+      if (+user.otp.expiresIn > now) {
+        throw createHttpError.Forbidden(
+          "کد احراز هویت قبلی هنوز منقضی نشده است"
+        );
+      }
+      return await this.updateUser(mobile, { otp });
+    }
+    return await UserModel.create({
+      mobile,
+      otp,
+      role: countOfRegisteredUser > 2 ? "USER" : "ADMIN",
+    });
+  }
+
+  async checkExistUser(mobile) {
+    const user = await UserModel.findOne({ mobile });
+    return user;
+  }
+
+  async updateUser(mobile, objectData = {}) {
+    Object.keys(objectData).forEach((key) => {
+      if (["", " ", "0", 0, null, undefined, NaN].includes(objectData[key]))
+        delete objectData[key];
+    });
+    const updateResult = await UserModel.updateOne(
+      { mobile },
+      { $set: objectData }
+    );
+
+    return !!updateResult.modifiedCount;
+  }
+}
+
+const AuthController = new Auth();
+
+module.exports = AuthController;
