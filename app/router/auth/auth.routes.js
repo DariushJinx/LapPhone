1ef0848cb8340acb970f83483bbb3714d714d8d7
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-29 16:40:43 +0330

    file updated for check otp and refresh token route

--- app/router/auth/auth.routes.js
+++ app/router/auth/auth.routes.js
@@ -3,5 +3,7 @@ const AuthController = require("../../http/controller/auth/auth.controller");
 const AuthRoutes = require("express").Router();
 
 AuthRoutes.post("/get-otp", AuthController.getOtp);
+AuthRoutes.post("/check-otp", AuthController.checkOtp);
+AuthRoutes.post("/refresh-token", AuthController.refreshToken);
 
 module.exports = AuthRoutes;
