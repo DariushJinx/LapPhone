0aac0583c93c16e615c5090a011197774ee28f32
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-29 16:42:08 +0330

    file updated

--- app/utils/functions.utils.js
+++ app/utils/functions.utils.js
@@ -1,9 +1,78 @@
+const JWT = require("jsonwebtoken");
+const UserModel = require("../http/models/user/user.model");
+const {
+  ACCESS_TOKEN_SECRET_KEY,
+  REFRESH_TOKEN_SECRET_KEY,
+} = require("./constans.utils");
+const createHttpError = require("http-errors");
+const redisClient = require("./initRedis.utils");
+
 function RandomNumberGenerator() {
   return ~~(Math.random() * 90000 + 10000);
 }
 
+function SignAccessToken(userID) {
+  return new Promise(async (resolve, reject) => {
+    const user = UserModel.findById(userID);
+    const payload = {
+      mobile: user.mobile,
+    };
+    const options = {
+      expiresIn: "1w",
+    };
+    JWT.sign(payload, ACCESS_TOKEN_SECRET_KEY, options, (err, token) => {
+      if (err) reject(createHttpError.InternalServerError("خطای سرور"));
+      resolve(token);
+    });
+  });
+}
+
+function SignRefreshToken(userID) {
+  return new Promise(async (resolve, reject) => {
+    const user = await UserModel.findById(userID);
+    const payload = {
+      mobile: user.mobile,
+    };
+    const options = {
+      expiresIn: "1y",
+    };
+    JWT.sign(payload, REFRESH_TOKEN_SECRET_KEY, options, async (err, token) => {
+      if (err) throw createHttpError.InternalServerError("خطای سرور");
+      await redisClient.SETEX(String(userID), 365 * 24 * 60 * 60, token);
+      resolve(token);
+    });
+  });
+}
+
+function verifyRefreshToken(token) {
+  return new Promise(async (resolve, reject) => {
+    JWT.verify(token, REFRESH_TOKEN_SECRET_KEY, async (err, payload) => {
+      if (err)
+        throw reject(createHttpError.Unauthorized("وارد حساب کاربری خود شوید"));
+      const { mobile } = payload || {};
+      const user = await UserModel.findOne({ mobile }, { password: 0, otp: 0 });
+      if (!user)
+        throw reject(
+          createHttpError.Unauthorized("حساب کاربری مورد نظر یافت نشد")
+        );
+      const refreshToken = await redisClient.get(String(user?._id));
+      if (!refreshToken)
+        throw reject(
+          createHttpError.Unauthorized("ورود مجدد به حساب کاربری انجام نشد")
+        );
+      if (refreshToken === token) return resolve(mobile);
+      reject(
+        createHttpError.Unauthorized("ورود مجدد به حساب کاربری انجام نشد")
+      );
+    });
+  });
+}
+
 const UtilsFunctions = {
   RandomNumberGenerator,
+  SignAccessToken,
+  SignRefreshToken,
+  verifyRefreshToken,
 };
 
 module.exports = UtilsFunctions;
