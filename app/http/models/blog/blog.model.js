4d214259e30ba6d6a0191daa92f4c0caea8103a2
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-30 22:06:15 +0330

    create and complete model for blog

---
+++ app/http/models/blog/blog.model.js
@@ -0,0 +1,50 @@
+const { default: mongoose } = require("mongoose");
+const CommentSchema = require("../pbublic/public.model");
+
+const BlogSchema = new mongoose.Schema(
+  {
+    author: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    short_text: { type: String, required: true },
+    images: { type: [String], required: true },
+    tags: { type: [String], default: [] },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "categories",
+      required: true,
+    },
+    comments: { type: [CommentSchema], default: [] },
+    likes: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
+    dislikes: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
+    bookmarks: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+BlogSchema.virtual("user_details", {
+  ref: "users",
+  localField: "_id",
+  foreignField: "author",
+});
+BlogSchema.virtual("category_details", {
+  ref: "categories",
+  localField: "_id",
+  foreignField: "category",
+});
+
+BlogSchema.virtual("images_URL").get(function () {
+  return this.images.map((image) => {
+    `${process.env.BASE_URL}:${process.env.APPLICATION_PORT}/${image}`;
+  });
+});
+
+const BlogModel = mongoose.model("blogs", BlogSchema);
+
+module.exports = BlogModel;
