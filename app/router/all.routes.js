b4165f7fc99aa3e1cf7163338331379ef0ab802e
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-30 22:09:03 +0330

    update all routes

--- app/router/all.routes.js
+++ app/router/all.routes.js
@@ -1,9 +1,13 @@
+
+const { verifyAccessToken } = require("../http/middlewares/verifyAccessToken.middleware");
+const AdminRoutes = require("./admin/main.admin.routes");
 const AuthRoutes = require("./auth/auth.routes");
 const HomeRoutes = require("./home/home.routes");
 
 const AllRoutes = require("express").Router();
 
-AllRoutes.use("/",HomeRoutes)
-AllRoutes.use("/auth",AuthRoutes)
+AllRoutes.use("/", HomeRoutes);
+AllRoutes.use("/auth", AuthRoutes);
+AllRoutes.use("/admin", verifyAccessToken, AdminRoutes);
 
 module.exports = AllRoutes;
