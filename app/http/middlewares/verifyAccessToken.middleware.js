cd33b7114e25b6f98c868deac5830c24812994eb
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-30 22:05:54 +0330

    handle verify access token for routes

---
+++ app/http/middlewares/verifyAccessToken.middleware.js
@@ -0,0 +1,48 @@
+const createHttpError = require("http-errors");
+const JWT = require("jsonwebtoken");
+const { ACCESS_TOKEN_SECRET_KEY } = require("../../utils/constans.utils");
+const UserModel = require("../models/user/user.model");
+
+function getToken(headers) {
+  const [bearer, token] = headers?.authorization?.split(" ") || [];
+  if (token && ["Bearer", "bearer"].includes(bearer)) return token;
+  throw createHttpError.Unauthorized(
+    "حساب کاربری مورد نظر یافت نشد || وارد حساب کاربری خود شوید"
+  );
+}
+
+function verifyAccessToken(req, res, next) {
+  try {
+    const token = getToken(req.headers);
+    JWT.verify(token, ACCESS_TOKEN_SECRET_KEY, (err, payload) => {
+      try {
+        if (err)
+          throw createHttpError.Unauthorized("وارد حساب کاربری خود شوید");
+        const { mobile } = payload || {};
+        const user = UserModel.findOne({ mobile }, { password: 0, otp: 0 });
+        if (!user)
+          throw createHttpError.Unauthorized("...وارد حساب کاربری خود شوید");
+        req.user = user;
+        return next();
+      } catch (err) {
+        next(err);
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+function checkRole(role) {
+  return function (req, res, next) {
+    try {
+      const user = req.user;
+      if (user.role.includes(role)) return next();
+      throw createHttpError.Forbidden("شما به این سطح دسترسی ندارید");
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+module.exports = {verifyAccessToken,checkRole};
