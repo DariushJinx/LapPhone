9d5abf7e96da89e48931efe7eb15e2829739c404
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-31 16:59:01 +0330

    create and complete swagger for public routes

--- app/router/admin/admin-swagger/public.swagger.js
+++ app/router/admin/admin-swagger/public.swagger.js
@@ -88,9 +88,9 @@
  *                                      example : the title of category
  *                                  children :
  *                                      type : array
- *                                      items : 
+ *                                      items :
  *                                          type : object
- *                                          properties : 
+ *                                          properties :
  *                                              _id :
  *                                                  type : string
  *                                                  example : "64c51bbdcc2bffaac359da5d"
@@ -100,9 +100,9 @@
  *                                              parent :
  *                                                  type : string
  *                                                  example : the id of parent category
- *                                              children : 
+ *                                              children :
  *                                                  type : array
- *                                                  items : 
+ *                                                  items :
  *                                                      type : object
  */
 
@@ -166,4 +166,81 @@
  *                                              description :
  *                                                 type : string
  *                                                 example : the best description
+ */
+
+/**
+ * @swagger
+ *  definitions :
+ *      CreateBlog :
+ *          type : object
+ *          properties :
+ *              statusCode :
+ *                  type : integer
+ *                  example : 20X
+ *              data :
+ *                  type : object
+ *                  properties :
+ *                      message :
+ *                          type : string
+ *                          example : the best message for route
+ *                      blog :
+ *                          type : array
+ *                          items :
+ *                              type : object
+ *                              properties :
+ *                                  _id :
+ *                                      type : string
+ *                                      example : "64c53b2e2eb15b78bb369ce9"
+ *                                  title :
+ *                                      type : string
+ *                                      example : the best title
+ *                                  text :
+ *                                      type : string
+ *                                      example : the best text
+ *                                  short_text :
+ *                                      type : string
+ *                                      example : the best short text
+ *                                  tags :
+ *                                      type : array
+ *                                      items :
+ *                                          type : string
+ *
+ *                                  category :
+ *                                      type : string
+ *                                      example : the id of category blog
+ *                                  author :
+ *                                      type : string
+ *                                      example : the id of author blog
+ *                                  images :
+ *                                      type : array
+ *                                      items :
+ *                                          type : string
+ */
+
+/**
+ * @swagger
+ *  definitions :
+ *      CreateMenu :
+ *          type : object
+ *          properties :
+ *              statusCode :
+ *                  type : integer
+ *                  example : 20X
+ *              data :
+ *                  type : object
+ *                  properties :
+ *                      message :
+ *                          type : string
+ *                          example : the best message for route
+ *                      menu :
+ *                          type : array
+ *                          items :
+ *                              type : object
+ *                              properties :
+ *                                  _id :
+ *                                      type : string
+ *                                      example : "64c7a63c69c2d3480160f729"
+ *                                  title :
+ *                                      type : string
+ *                                      example : the title of menu
  */
