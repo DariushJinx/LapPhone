d94cb6664129048f81b3002f70dfc743328d1256
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-31 16:55:15 +0330

    file updated

--- app/http/middlewares/permission.guard.js
+++ app/http/middlewares/permission.guard.js
@@ -6,17 +6,17 @@ const RoleModel = require("../models/role/role.model");
 function checkPermission(requiredPermissions = []) {
   return async function (req, res, next) {
     try {
-      const allPermissions = requiredPermissions.flat(2)
+      const allPermissions = requiredPermissions.flat(2);
       let user = req.user;
-      console.log("Roles : " , user.role)
-      const role = await RoleModel.findOne({title: user.role})
-      console.log("role : " , role)
-      const permissions = await PermissionModel.find({_id: {$in : role.permissions}})
-      const userPermissions = permissions.map(item => item.name)
-      const hasPermission = allPermissions.every(permission => {
-          return userPermissions.includes(permission)
-      })
-      if(userPermissions.includes(PERMISSIONS.ALL)) return next()
+      const role = await RoleModel.findOne({ title: user.role });
+      const permissions = await PermissionModel.find({
+        _id: { $in: role.permissions },
+      });
+      const userPermissions = permissions.map((item) => item.name);
+      const hasPermission = allPermissions.every((permission) => {
+        return userPermissions.includes(permission);
+      });
+      if (userPermissions.includes(PERMISSIONS.ALL)) return next();
       if (allPermissions.length == 0 || hasPermission) return next();
       throw createHttpError.Forbidden("شما به این قسمت دسترسی ندارید");
     } catch (error) {
