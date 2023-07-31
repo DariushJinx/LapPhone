afefe06fcea70e6b0d2bb9594665510bd8b512fe
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-31 16:58:09 +0330

    file updated for permission routes

--- app/router/admin/main.admin.routes.js
+++ app/router/admin/main.admin.routes.js
@@ -1,11 +1,23 @@
+const checkPermission = require("../../http/middlewares/permission.guard");
+const { PERMISSIONS } = require("../../utils/constans.utils");
+const BlogRoutes = require("./blog/blog.routes");
 const CategoryRoutes = require("./category/category.routes");
 const PermissionRoutes = require("./permission/permissioin.routes");
 const RoleRoutes = require("./role/role.routes");
 
 const AdminRoutes = require("express").Router();
 
-AdminRoutes.use("/permission", PermissionRoutes);
-AdminRoutes.use("/role", RoleRoutes);
-AdminRoutes.use("/category", CategoryRoutes);
+AdminRoutes.use(
+  "/permission",
+  checkPermission([PERMISSIONS.ADMIN]),
+  PermissionRoutes
+);
+AdminRoutes.use("/role", checkPermission([PERMISSIONS.ADMIN]), RoleRoutes);
+AdminRoutes.use(
+  "/category",
+  checkPermission([PERMISSIONS.ADMIN]),
+  CategoryRoutes
+);
+AdminRoutes.use("/blog", checkPermission([PERMISSIONS.USER]), BlogRoutes);
 
 module.exports = AdminRoutes;
