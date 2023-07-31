6002a65d774c142109e2e759dc9023a378c21136
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-31 16:58:48 +0330

    create and complete swagger for menu

---
+++ app/router/admin/admin-swagger/menu.swagger.js
@@ -0,0 +1,107 @@
+/**
+ * @swagger
+ *  components :
+ *      schemas :
+ *          CreateMenu :
+ *              type : object
+ *              required :
+ *                  -   title
+ *              properties :
+ *                  title :
+ *                      type : string
+ *                      description : the title of menu
+ *          UpdateMenu :
+ *              type : object
+ *              properties :
+ *                  title :
+ *                      type : string
+ *                      description : the title of menu
+ */
+
+/**
+ * @swagger
+ *  /menu/add :
+ *      post :
+ *          tags : [Menu]
+ *          summary : create menu
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/CreateMenu'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/CreateMenu'
+ *          responses :
+ *              201 :
+ *                  description : create - menu created
+ *                  content :
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/CreateMenu'
+ */
+
+/**
+ * @swagger
+ *  /menu/list :
+ *      get :
+ *          tags : [Menu]
+ *          summary : get all menus
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  content :
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/CreateMenu'
+ */
+
+/**
+ * @swagger
+ *  /menu/update/{field} :
+ *      patch :
+ *          tags : [Menu]
+ *          summary : update menu with id or title
+ *          parameters :
+ *              -   in : path
+ *                  name : field
+ *                  type : string
+ *                  required : true
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/UpdateMenu'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/UpdateMenu'
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  content :
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/DeleteAndUpdate'
+ */
+
+/**
+ * @swagger
+ *  /menu/remove/{field} :
+ *      delete :
+ *          tags : [Menu]
+ *          summary : remove menu with id or title
+ *          parameters :
+ *              -   in : path
+ *                  name : field
+ *                  type : string
+ *                  required : true
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  content :
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/DeleteAndUpdate'
+ */
