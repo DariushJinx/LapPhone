85d6df16d401cf77091b8dc60e422c02c9ba15c1
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-04 23:47:32 +0330

    create and complete route for product

---
+++ app/router/admin/product/product.routes.js
@@ -0,0 +1,38 @@
+const ProductController = require("../../../http/controller/product/product.controller");
+const stringToArray = require("../../../http/middlewares/stringToArray");
+const { uploadFile } = require("../../../utils/multer.utils");
+
+const ProductRoutes = require("express").Router();
+
+ProductRoutes.post(
+  "/add",
+  uploadFile.array("images", 10),
+  stringToArray("tags", "colors"),
+  ProductController.addProduct
+);
+ProductRoutes.get("/list", ProductController.listOfProduct);
+ProductRoutes.get("/search", ProductController.searchOfProduct);
+ProductRoutes.patch(
+  "/add-features/:field",
+  ProductController.addFeaturesForProduct
+);
+ProductRoutes.delete(
+  "/remove-feature/:productID",
+  ProductController.removeFeature
+);
+ProductRoutes.delete("/remove-product/:field", ProductController.removeProduct);
+ProductRoutes.patch(
+  "/update-product/:field",
+  uploadFile.array("images", 10),
+  stringToArray("tags", "colors"),
+  ProductController.updateProduct
+);
+ProductRoutes.get("/:field", ProductController.getOneProduct);
+ProductRoutes.get("/like/:productID", ProductController.likedProduct);
+ProductRoutes.get("/dislike/:productID", ProductController.dislikedProduct);
+ProductRoutes.get("/bookmark/:productID", ProductController.bookmarkedProduct);
+ProductRoutes.post(
+  "/create-comment/:productID",
+  ProductController.createCommentForProduct
+);
+module.exports = ProductRoutes;
