81ffd4c86e2bcee9b21feee881527e32d2c01783
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-07 11:50:34 +0330

    create and complete validation for comment

---
+++ app/http/validation/comment/comment.validation.js
@@ -0,0 +1,17 @@
+const Joi = require("@hapi/joi");
+const createHttpError = require("http-errors");
+const { MongoIDPattern } = require("../../../utils/constans.utils");
+
+const CreateCommentValidation = Joi.object({
+  comment: Joi.string().error(
+    createHttpError.BadRequest("کامنت ارسالی صحیح نمی باشد")
+  ),
+  blogName: Joi.string().error(
+    createHttpError.BadRequest("عنوان ارسالی مقاله صحیح نمی باشد")
+  ),
+  productName: Joi.string().error(
+    createHttpError.BadRequest("عنوان ارسالی محصول صحیح نمی باشد")
+  ),
+});
+
+module.exports = CreateCommentValidation;
