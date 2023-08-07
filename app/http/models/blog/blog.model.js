10d3aadf30aebfcb702c57b200a5b09042397915
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-07 11:48:38 +0330

    file updated with remove comment

--- app/http/models/blog/blog.model.js
+++ app/http/models/blog/blog.model.js
@@ -1,5 +1,4 @@
 const { default: mongoose } = require("mongoose");
-const CommentSchema = require("../pbublic/public.model");
 
 const BlogSchema = new mongoose.Schema(
   {
@@ -14,7 +13,6 @@ const BlogSchema = new mongoose.Schema(
       ref: "categories",
       required: true,
     },
-    comments: { type: [CommentSchema], default: [] },
     likes: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
     dislikes: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
     bookmarks: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
@@ -27,40 +25,7 @@ const BlogSchema = new mongoose.Schema(
     },
   }
 );
-
-BlogSchema.virtual("user_details", {
-  ref: "users",
-  localField: "_id",
-  foreignField: "author",
-});
-BlogSchema.virtual("category_details", {
-  ref: "categories",
-  localField: "_id",
-  foreignField: "category",
-});
-BlogSchema.virtual("bookmark_details", {
-  ref: "users",
-  localField: "_id",
-  foreignField: "bookmarks",
-});
-BlogSchema.virtual("likes_details", {
-  ref: "users",
-  localField: "_id",
-  foreignField: "likes",
-});
-BlogSchema.virtual("dislikes_details", {
-  ref: "users",
-  localField: "_id",
-  foreignField: "dislikes",
-});
-
-BlogSchema.virtual("imagesURL").get(function () {
-  return this.images.map(
-    (image) =>
-      `${process.env.BASE_URL}:${process.env.APPLICATION_PORT}/${image}`
-  );
-});
-
+BlogSchema.index({ title: "text", text: "text", short_text: "text" });
 const BlogModel = mongoose.model("blogs", BlogSchema);
 
 module.exports = BlogModel;
