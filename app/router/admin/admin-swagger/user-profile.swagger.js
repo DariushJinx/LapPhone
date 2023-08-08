62f794cd8678be86597827b1985eb6a3bf5c707e
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-08 21:27:03 +0330

    create and complete swagger for user profile

---
+++ app/router/admin/admin-swagger/user-profile.swagger.js
@@ -0,0 +1,30 @@
+/**
+ * @swagger
+ *  /profile/bookmark-blog : 
+ *      get : 
+ *          tags : [UserProfile]
+ *          summary : get bookmark blog of user
+ *          responses : 
+ *              200 : 
+ *                  description : success
+ */
+/**
+ * @swagger
+ *  /profile/bookmark-product : 
+ *      get : 
+ *          tags : [UserProfile]
+ *          summary : get bookmark product of user
+ *          responses : 
+ *              200 : 
+ *                  description : success
+ */
+/**
+ * @swagger
+ *  /profile/basket : 
+ *      get : 
+ *          tags : [UserProfile]
+ *          summary : get basket of user
+ *          responses : 
+ *              200 : 
+ *                  description : success
+ */
