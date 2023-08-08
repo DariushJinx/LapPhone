8810acfe7eff77d9ffe4ab1846cdcf71a70e7e7c
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-08 21:27:16 +0330

    file updated

--- app/router/admin/admin-swagger/user.swagger.js
+++ app/router/admin/admin-swagger/user.swagger.js
@@ -21,6 +21,20 @@
  *                      type : string
  *                      description : birthday of user
  */
+/**
+ * @swagger
+ *  components :
+ *      schemas :
+ *          ChangeRole :
+ *              type : object
+ *              properties :
+ *                  id :
+ *                      type : string
+ *                      description : the id of user
+ *                  role :
+ *                      type : string
+ *                      description : the role of user
+ */
 
 /**
  * @swagger
@@ -41,6 +55,25 @@
  *              200 :
  *                  description : success
  */
+/**
+ * @swagger
+ *  /user/update-role :
+ *      patch :
+ *          tags : [User]
+ *          summary : change role of user
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/ChangeRole'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/ChangeRole'
+ *          responses :
+ *              200 :
+ *                  description : success
+ */
 
 /**
  * @swagger
@@ -53,6 +86,22 @@
  *                  name : search
  *                  type : string
  *                  description: search in user first_name, last_name, username, mobile, email
+ *          responses :
+ *              200 :
+ *                  description : success
+ */
+
+/**
+ * @swagger
+ *  /user/ban/{id} :
+ *      post :
+ *          tags : [User]
+ *          summary : ban user
+ *          parameters :
+ *              -   in : path
+ *                  name : id
+ *                  type : string
+ *                  required : true
  *          responses :
  *              200 :
  *                  description : success
