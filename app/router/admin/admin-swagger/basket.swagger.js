8b83b7ba18baffff1f048ade09545b497e68f6c1
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-08 21:26:13 +0330

    create and complete swagger for basket

---
+++ app/router/admin/admin-swagger/basket.swagger.js
@@ -0,0 +1,30 @@
+/**
+ * @swagger
+ *  /basket/add-product/{productID} :
+ *      patch :
+ *          tags : [Basket]
+ *          summary : add product in basket
+ *          parameters :
+ *              -   in : path
+ *                  name : productID
+ *                  type : string
+ *                  required : true
+ *          responses :
+ *              200 :
+ *                  description : success
+ */
+/**
+ * @swagger
+ *  /basket/remove-product/{productID} :
+ *      patch :
+ *          tags : [Basket]
+ *          summary : remove product from basket
+ *          parameters :
+ *              -   in : path
+ *                  name : productID
+ *                  type : string
+ *                  required : true
+ *          responses :
+ *              200 :
+ *                  description : success
+ */
