6b9bf1eea291cf70edb6851bb5fd978b64659ca1
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-08 21:28:27 +0330

    file updated

--- app/router/user/user.routes.js
+++ app/router/user/user.routes.js
@@ -3,6 +3,8 @@ const UserController = require("../../http/controller/user/user.controller");
 const UserRoutes = require("express").Router();
 
 UserRoutes.patch("/update-profile", UserController.updateUserProfile);
+UserRoutes.patch("/update-role", UserController.changeRoles);
+UserRoutes.post("/ban/:id", UserController.banUser);
 UserRoutes.get("/list", UserController.getAllUsers);
 
 module.exports = UserRoutes;
