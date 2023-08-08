5ca66bde076c02446110c88f9a702deca8ff9140
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-08 21:25:47 +0330

    file updated

--- app/router/all.routes.js
+++ app/router/all.routes.js
@@ -4,9 +4,11 @@ const {
 const commentRoutes = require("./admin/comment/comment.routes");
 const AdminRoutes = require("./admin/main.admin.routes");
 const AuthRoutes = require("./auth/auth.routes");
+const BasketRoutes = require("./basket/basket.routes");
 const ContactRoutes = require("./contact/contact.routes");
 const HomeRoutes = require("./home/home.routes");
 const MenuRoutes = require("./menu/menu.routes");
+const ProfileRoutes = require("./user-profile/user-profile.routes");
 const UserRoutes = require("./user/user.routes");
 
 const AllRoutes = require("express").Router();
@@ -18,5 +20,7 @@ AllRoutes.use("/menu", MenuRoutes);
 AllRoutes.use("/user", verifyAccessToken, UserRoutes);
 AllRoutes.use("/comment", verifyAccessToken, commentRoutes);
 AllRoutes.use("/contact", verifyAccessToken, ContactRoutes);
+AllRoutes.use("/basket", verifyAccessToken, BasketRoutes);
+AllRoutes.use("/profile", verifyAccessToken, ProfileRoutes);
 
 module.exports = AllRoutes;
