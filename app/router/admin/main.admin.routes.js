9b46a2476eb9431e94c345c2d3eb3263fc55cd95
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-09 09:15:26 +0330

    file update

--- app/router/admin/main.admin.routes.js
+++ app/router/admin/main.admin.routes.js
@@ -2,6 +2,8 @@ const checkPermission = require("../../http/middlewares/permission.guard");
 const { PERMISSIONS } = require("../../utils/constans.utils");
 const BlogRoutes = require("./blog/blog.routes");
 const CategoryRoutes = require("./category/category.routes");
+const DepartmentRoutes = require("./department/department.routes");
+const departmentSubRoutes = require("./departmentSub/department-sub.routes");
 const OffRoutes = require("./off/off.routes");
 const PermissionRoutes = require("./permission/permissioin.routes");
 const ProductRoutes = require("./product/product.routes");
@@ -23,5 +25,7 @@ AdminRoutes.use(
 AdminRoutes.use("/blog", checkPermission([PERMISSIONS.USER]), BlogRoutes);
 AdminRoutes.use("/product", ProductRoutes);
 AdminRoutes.use("/off", OffRoutes);
+AdminRoutes.use("/department", DepartmentRoutes);
+AdminRoutes.use("/departmentSub", departmentSubRoutes);
 
 module.exports = AdminRoutes;
