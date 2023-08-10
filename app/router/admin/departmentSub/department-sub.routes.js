6204980f404e4b3bdfcdf585c8b1c7e9624ec9fc
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-10 17:10:57 +0330

    file  updated

--- app/router/admin/departmentSub/department-sub.routes.js
+++ app/router/admin/departmentSub/department-sub.routes.js
@@ -1,7 +1,16 @@
 const DepartmentSubController = require("../../../http/controller/departmentSub/department-sub.controller");
 
-const departmentSubRoutes = require("express").Router();
+const DepartmentSubRoutes = require("express").Router();
 
-departmentSubRoutes.post("/add", DepartmentSubController.createDepartmentSub);
+DepartmentSubRoutes.post("/add", DepartmentSubController.createDepartmentSub);
+DepartmentSubRoutes.get("/list", DepartmentSubController.listOfDepartmentSub);
+DepartmentSubRoutes.patch(
+  "/update/:field",
+  DepartmentSubController.updateDepartmentSub
+);
+DepartmentSubRoutes.delete(
+  "/remove/:field",
+  DepartmentSubController.removeDepartmentSub
+);
 
-module.exports = departmentSubRoutes;
+module.exports = DepartmentSubRoutes;
