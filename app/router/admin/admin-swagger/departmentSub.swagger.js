81a2a038b8145d067fb4359e54cf810cf4905bc4
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-10 17:10:00 +0330

    file updated for update and list departmentSub

--- app/router/admin/admin-swagger/departmentSub.swagger.js
+++ app/router/admin/admin-swagger/departmentSub.swagger.js
@@ -15,6 +15,19 @@
  *                      type : string
  *                      description : the id of department
  */
+/**
+ * @swagger
+ *  components :
+ *      schemas :
+ *          UpdateDepartmentSub :
+ *              type : object
+ *              required :
+ *                  -   title
+ *              properties :
+ *                  title :
+ *                      type : string
+ *                      description : title for update department sub
+ */
 
 /**
  * @swagger
@@ -34,4 +47,53 @@
  *          responses :
  *              201 :
  *                  description : create - departmentSub created
+ */
+/**
+ * @swagger
+ *  /admin/departmentSub/list :
+ *      get :
+ *          tags : [DepartmentSub]
+ *          summary : get list of departmentSubs
+ *          responses :
+ *              200 :
+ *                  description : success 
+ */
+/**
+ * @swagger
+ *  /admin/departmentSub/update/{field} :
+ *      patch :
+ *          tags : [DepartmentSub]
+ *          summary : create departmentSub
+ *          parameters :
+ *              -   in : path
+ *                  name : field
+ *                  type : string
+ *                  required : true
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/UpdateDepartmentSub'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/UpdateDepartmentSub'
+ *          responses :
+ *              200 :
+ *                  description : success - departmentSub updated
+ */
+/**
+ * @swagger
+ *  /admin/departmentSub/remove/{field} :
+ *      delete :
+ *          tags : [DepartmentSub]
+ *          summary : create departmentSub
+ *          parameters :
+ *              -   in : path
+ *                  name : field
+ *                  type : string
+ *                  required : true
+ *          responses :
+ *              200 :
+ *                  description : success - departmentSub removed
  */
