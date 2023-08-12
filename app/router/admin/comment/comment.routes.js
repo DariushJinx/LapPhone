b144a70fd0bfb30262fb555db5c0f8757a367216
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-12 18:23:50 +0330

    file updated

--- app/router/admin/comment/comment.routes.js
+++ app/router/admin/comment/comment.routes.js
@@ -1,14 +1,28 @@
 const CommentController = require("../../../http/controller/comment/comment.controller");
+const checkPermission = require("../../../http/middlewares/permission.guard");
 const {
   verifyAccessToken,
 } = require("../../../http/middlewares/verifyAccessToken.middleware");
+const { PERMISSIONS } = require("../../../utils/constans.utils");
 
 const commentRoutes = require("express").Router();
 
 commentRoutes.post("/add", CommentController.createComment);
-commentRoutes.get("/list", CommentController.getAllComments);
-commentRoutes.patch("/show/:id", CommentController.showComment);
+commentRoutes.get(
+  "/list",
+  checkPermission([PERMISSIONS.ADMIN]),
+  CommentController.getAllComments
+);
+commentRoutes.patch(
+  "/show/:id",
+  checkPermission([PERMISSIONS.ADMIN]),
+  CommentController.showComment
+);
 commentRoutes.patch("/answer/:id", CommentController.createAnswer);
-commentRoutes.delete("/remove/:id", CommentController.removeComment);
+commentRoutes.delete(
+  "/remove/:id",
+  checkPermission([PERMISSIONS.ADMIN]),
+  CommentController.removeComment
+);
 
 module.exports = commentRoutes;
